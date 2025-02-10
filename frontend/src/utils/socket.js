e6ed07e1847c05417fe6ad92c8502c93dd7aacf9
codder077 <120477792+codder077@users.noreply.github.com>
2025-02-10 14:18:35 +0530

    deploy changes

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
-const SOCKET_SERVER_URL = "http://localhost:5000"; 
-const socket = io(`${SOCKET_SERVER_URL}`, {
-    transports: ["websocket"],
+const socket = io(`${process.env.REACT_APP_SOCKET_SERVER_URL}`, {
+  transports: ["websocket"],
 });
+
 
 socket.on('connect', () => {
   console.log('Socket connected');
